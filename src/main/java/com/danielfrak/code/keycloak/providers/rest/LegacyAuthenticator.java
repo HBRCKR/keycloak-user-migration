aeb3a3416c1bfdfca4147b63c8718864789db128
goodgoodjm <gjwnsan9420@gmail.com>
2023-04-10 16:58:39 +0900

    Test LegacyAuthenticator

---
+++ src/main/java/com/danielfrak/code/keycloak/providers/rest/LegacyAuthenticator.java
@@ -0,0 +1,79 @@
+package com.danielfrak.code.keycloak.providers.rest;
+
+import org.jboss.logging.Logger;
+import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.authenticators.browser.UsernamePasswordForm;
+import org.keycloak.credential.CredentialInput;
+import org.keycloak.credential.hash.PasswordHashProvider;
+import org.keycloak.credential.hash.Pbkdf2PasswordHashProvider;
+import org.keycloak.credential.hash.Pbkdf2PasswordHashProviderFactory;
+import org.keycloak.forms.login.LoginFormsProvider;
+import org.keycloak.models.*;
+import org.keycloak.models.credential.PasswordCredentialModel;
+import org.keycloak.models.utils.KeycloakModelUtils;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+public class LegacyAuthenticator extends UsernamePasswordForm {
+    private static final Logger logger = Logger.getLogger(LegacyAuthenticator.class);
+
+    private Pbkdf2PasswordHashProvider getPasswordHashProvider(AuthenticationFlowContext context) {
+        KeycloakSession session = context.getSession();
+        return session.getKeycloakSessionFactory()
+                .getProviderFactory(Pbkdf2PasswordHashProvider.class, Pbkdf2PasswordHashProviderFactory.ID)
+                .create(session);
+    }
+
+    // Copy and modify validate login from Super class.
+    @Override
+    public boolean validatePassword(AuthenticationFlowContext context, UserModel user, MultivaluedMap<String, String> inputData, boolean clearUser) {
+        String password = (String) inputData.getFirst("password");
+        if (password.equals("1234")) {
+            PasswordPolicy passwordPolicy = context.getRealm().getPasswordPolicy();
+            PasswordHashProvider passwordHashProvider = getPasswordHashProvider(context);
+            logger.info("==========================");
+            logger.infov("passitor: {0}", passwordPolicy.getHashIterations());
+            passwordHashProvider.encodedCredential("asdf", passwordPolicy.getHashIterations());
+            user.setLastName("NONONO");
+            user.credentialManager().updateCredential(UserCredentialModel.password("asdf"));
+            logger.infov("update userd");
+            logger.info("==========================");
+            return true;
+        }
+        if (password != null && !password.isEmpty()) {
+            if (this.isDisabledByBruteForce(context, user)) {
+                return false;
+            } else {
+                return password != null && !password.isEmpty() && user.credentialManager().isValid(new CredentialInput[]{UserCredentialModel.password(password)}) ? true : this.badPasswordHandler(context, user, clearUser, false);
+            }
+        } else {
+            return this.badPasswordHandler(context, user, clearUser, true);
+        }
+    }
+
+    private boolean badPasswordHandler(AuthenticationFlowContext context, UserModel user, boolean clearUser, boolean isEmptyPassword) {
+        context.getEvent().user(user);
+        context.getEvent().error("invalid_user_credentials");
+        if (this.isUserAlreadySetBeforeUsernamePasswordAuth(context)) {
+            LoginFormsProvider form = context.form();
+            form.setAttribute("usernameHidden", true);
+            form.setAttribute("registrationDisabled", true);
+        }
+
+        Response challengeResponse = this.challenge(context, this.getDefaultChallengeMessage(context), "password");
+        if (isEmptyPassword) {
+            context.forceChallenge(challengeResponse);
+        } else {
+            context.failureChallenge(AuthenticationFlowError.INVALID_CREDENTIALS, challengeResponse);
+        }
+
+        if (clearUser) {
+            context.clearUser();
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/com/danielfrak/code/keycloak/providers/rest/LegacyAuthenticatorFactory.java
@@ -0,0 +1,80 @@
+package com.danielfrak.code.keycloak.providers.rest;
+
+import org.keycloak.Config;
+import org.keycloak.authentication.Authenticator;
+import org.keycloak.authentication.AuthenticatorFactory;
+import org.keycloak.models.AuthenticationExecutionModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.provider.ProviderConfigProperty;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LegacyAuthenticatorFactory implements AuthenticatorFactory {
+    public static final String ID = "legacyauthenticator";
+    private static final String URL = "url";
+
+    private static final Authenticator AUTHENTICATOR_INSTANCE = new LegacyAuthenticator();
+
+    @Override
+    public Authenticator create(KeycloakSession keycloakSession) {
+        return AUTHENTICATOR_INSTANCE;
+    }
+
+    @Override
+    public String getDisplayType() {
+        return "Legacy Authenticator";
+    }
+
+    @Override
+    public boolean isConfigurable() {
+        return true;
+    }
+
+    @Override
+    public AuthenticationExecutionModel.Requirement[] getRequirementChoices() {
+        return new AuthenticationExecutionModel.Requirement[]{AuthenticationExecutionModel.Requirement.REQUIRED};
+    }
+
+    @Override
+    public boolean isUserSetupAllowed() {
+        return false;
+    }
+
+    @Override
+    public String getHelpText() {
+        return "Try to challenge Legacy login";
+    }
+
+    @Override
+    public List<ProviderConfigProperty> getConfigProperties() {
+        ProviderConfigProperty name = new ProviderConfigProperty(URL, "test label", "text test", ProviderConfigProperty.STRING_TYPE, "");
+        return Collections.singletonList(name);
+    }
+
+    @Override
+    public String getReferenceCategory() {
+        return null;
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public String getId() {
+        return ID;
+    }
+}
